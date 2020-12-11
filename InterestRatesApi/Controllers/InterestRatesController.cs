ed76d4ea464998de8a1949a6734b71fced6c8736
aismaniotto <aismaniotto@gmail.com>
2020-12-10 21:34:13 -0300

    feat(interestRate): create interest rate api with fix value

---
+++ InterestRatesApi/Controllers/InterestRatesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InterestRates.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InterestRatesController : ControllerBase
+    {
+        [HttpGet]
+        public double Get()
+        {
+            return 0.01;
+        }
+    }
+}
