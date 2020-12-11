dc385db4d0bccaf1471b69cbbaa7940cde2a970a
aismaniotto <aismaniotto@gmail.com>
2020-12-10 22:18:55 -0300

    feat(showMeTheCode): create api to return github repo

---
+++ CalculateInterestApi/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculateInterestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "https://github.com/aismaniotto/InterestRatesApi";
+        }
+    }
+}
