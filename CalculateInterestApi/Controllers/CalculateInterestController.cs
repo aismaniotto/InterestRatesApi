b11bbe1787fb5abab285c0da1c476b9a0c4ece0b
aismaniotto <aismaniotto@gmail.com>
2020-12-10 21:57:01 -0300

    feat(calculateInterest): create basic api to calculate interest

---
+++ CalculateInterestApi/Controllers/CalculateInterestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalculateInterestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculateInterestController : ControllerBase
+    {
+        [HttpGet]
+        public double Get(double initValue, int months)
+        {
+            return initValue * Math.Pow((1 + 0.01), months);
+        }
+    }
+}
