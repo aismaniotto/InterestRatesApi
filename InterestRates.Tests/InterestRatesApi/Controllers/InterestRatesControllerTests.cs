f428849c793e50223b1dd175378d733a66d1a036
aismaniotto <aismaniotto@gmail.com>
2020-12-12 20:48:38 -0300

    test(interestRates): setup test project and create test

---
+++ InterestRates.Tests/InterestRatesApi/Controllers/InterestRatesControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using InterestRates.Controllers;
+using NUnit.Framework;
+
+namespace InterestRates.Tests.InterestRatesApi.Controllers
+{
+    public class InterestRatesControllerTests
+    {
+        public InterestRatesController Controller { get; set; }
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            Controller = new InterestRatesController();
+        }
+
+        [Test]
+        public void Should_Return_Constant_Value_From_Interest_Rates_End_Point()
+        {
+            var constValueExpected = 0.01;
+            var interestRate = Controller.Get();
+            Assert.AreEqual(constValueExpected, interestRate);
+        }
+    }
+}
