afa68776326ed97a90f007b242a671f2a75b03d9
aismaniotto <aismaniotto@gmail.com>
2020-12-13 19:19:05 -0300

    test(calculateInterest): add tests

---
+++ InterestRates.Tests/CalculateInterestApi/Controllers/CalculateInterestControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CalculateInterestApi.Controllers;
+using CalculateInterestApi.Mocks;
+using NUnit.Framework;
+
+namespace InterestRates.Tests.CalculateInterestApi.Controllers
+{
+    public class CalculateInterestControllerTests
+    {
+        public CalculateInterestController Controller { get; set; }
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            Controller = new CalculateInterestController(new InterestRatesServiceMock());
+        }
+
+        [Test]
+        public void Should_Return_The_Interest_Rates_Acording_The_Parameters()
+        {
+            Assert.AreEqual(105.1, Controller.Get(100, 5));
+            Assert.AreEqual(563.41, Controller.Get(500, 12));
+        }
+    }
+}
