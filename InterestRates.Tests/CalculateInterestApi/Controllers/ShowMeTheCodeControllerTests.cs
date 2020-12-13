2309e4fd0bf1caac4615ff484b42cf61d2f02579
aismaniotto <aismaniotto@gmail.com>
2020-12-13 19:00:41 -0300

    test(showMeTheCode): add test

---
+++ InterestRates.Tests/CalculateInterestApi/Controllers/ShowMeTheCodeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CalculateInterestApi.Controllers;
+using NUnit.Framework;
+
+namespace InterestRates.Tests.CalculateInterestApi.Controllers
+{
+    public class ShowMeTheCodeControllerTests
+    {
+        public ShowMeTheCodeController Controller { get; set; }
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            Controller = new ShowMeTheCodeController();
+        }
+
+        [Test]
+        public void Should_Return_Repo_Address_From_Show_Me_The_Code_End_Point()
+        {
+            var repoAddressExpected = "https://github.com/aismaniotto/InterestRatesApi";
+            var repoAddress = Controller.Get();
+            Assert.AreEqual(repoAddressExpected, repoAddress);
+        }
+    }
+}
